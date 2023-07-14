5fc6087ec641d8cd6e3024169775aa8182c74019
omarebid1 <omar3bid18@gmail.com>
2023-07-15 00:00:53 +0200

    classe creato

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,7 +1,6 @@
 package com.ferragnez.party;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class CheckGuest {
 
@@ -9,7 +8,27 @@ public class CheckGuest {
 
         Scanner scanner = new Scanner(System.in);
 
+        // Dua Lipa, Paris Hilton, Manuel Agnelli, J-Ax, Francesco Totti, Ilary Blasi,
+        // Bebe Vio, Luis, Pardis Zarei, Martina Maccherone, Rachel Zeilic
 
+        String[] invitiedGuests = {"Dua Lipa", "Paris Hilton", "Manuel Angelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+
+
+
+        System.out.println("Enter Guest's full name");
+        String name = scanner.nextLine();
+
+        for (int i = 0; i <= invitiedGuests.length; i++){
+            if(name.equals(invitiedGuests[i])){
+                System.out.println("Welcome to our party dear Guest");
+
+            }
+
+            else{
+                System.out.println("Sorry u are not allowed to enter");
+                break;
+            }
+        }
 
     }
 
