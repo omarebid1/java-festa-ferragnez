ea8dfbca688591b37550a98ea6a5a59bd40dd7a3
omarebid1 <omar3bid18@gmail.com>
2023-07-14 23:40:10 +0200

    classe creato

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,8 +1,15 @@
 package com.ferragnez.party;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class CheckGuest {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
 
     }
 
